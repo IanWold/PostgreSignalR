b3992ada48645304edd7b8b4291502fe5d3ae302
ianwold <ilwold@hotmail.com>
2025-12-20 11:13:42 -0600

    Try more robust hashing

--- src/PostgresChannels.cs
+++ src/PostgresChannels.cs
@@ -1,4 +1,7 @@
+using System.Buffers.Text;
 using System.Runtime.CompilerServices;
+using System.Security.Cryptography;
+using System.Text;
 
 namespace PostgreSignalR;
 
@@ -6,15 +9,30 @@ internal sealed class PostgresChannels(string prefix, string returnServerName)
 {
     private const int _maxIdentifierLength = 63;
 
-    private static string Normalize(string name)
+    public static string Normalize(string prefix, string name)
     {
-        name = name.Replace('-', '_');
+        var channelName = $"{prefix}{name}";
+        Span<byte> utf8 = stackalloc byte[1024];
+        int utf8Length = Encoding.UTF8.GetBytes(channelName.AsSpan(), utf8);
 
-        var hash = Convert.ToHexString(System.Security.Cryptography.SHA256.HashData(System.Text.Encoding.UTF8.GetBytes(name))).ToLowerInvariant();
-        var suffix = $"_{hash[..8]}";
-        var trimLength = Math.Min(Math.Max(_maxIdentifierLength - suffix.Length, 0), name.Length);
+        Span<byte> hash = stackalloc byte[48];
+        SHA384.HashData(utf8[..utf8Length], hash);
 
-        return $"{name[..trimLength]}{suffix}";
+        Span<byte> base64 = stackalloc byte[64];
+        Base64.EncodeToUtf8(hash[..47], base64, out _, out int written);
+
+        return string.Create(written - 1, base64, static (destination, source) =>
+        {
+            for (int i = 0; i < destination.Length; i++)
+            {
+                destination[i] = source[i] switch
+                {
+                    (byte)'+' => '_',
+                    (byte)'/' => '_',
+                    var c => (char)c
+                };
+            }
+        });
     }
 
     /// <summary>
@@ -24,43 +42,43 @@ internal sealed class PostgresChannels(string prefix, string returnServerName)
     /// The payload on this channel is <see cref="PostgresInvocation"/> objects containing
     /// invocations to be sent to all connections
     /// </remarks>
-    public string All { get; } = Normalize($"{prefix}_all");
+    public string All { get; } = Normalize(prefix, "all");
 
     /// <summary>
     /// Gets the name of the internal channel for group management messages.
     /// </summary>
-    public string GroupManagement { get; } = Normalize($"{prefix}_internal_groups");
+    public string GroupManagement { get; } = Normalize(prefix, "internal_groups");
 
     /// <summary>
     /// Gets the name of the internal channel for receiving client results.
     /// </summary>
-    public string ReturnResults { get; } = Normalize($"{prefix}_internal_return_{returnServerName}");
+    public string ReturnResults { get; } = Normalize(prefix, $"internal_return_{returnServerName}");
 
     /// <summary>
     /// Gets the name of the channel for sending a message to a specific connection.
     /// </summary>
     /// <param name="connectionId">The ID of the connection to get the channel for.</param>
     [MethodImpl(MethodImplOptions.AggressiveInlining)]
-    public string Connection(string connectionId) => Normalize($"{prefix}_connection_{connectionId}");
+    public string Connection(string connectionId) => Normalize(prefix, $"connection_{connectionId}");
 
     /// <summary>
     /// Gets the name of the channel for sending a message to a named group of connections.
     /// </summary>
     /// <param name="groupName">The name of the group to get the channel for.</param>
     [MethodImpl(MethodImplOptions.AggressiveInlining)]
-    public string Group(string groupName) => Normalize($"{prefix}_group_{groupName}");
+    public string Group(string groupName) => Normalize(prefix, $"group_{groupName}");
 
     /// <summary>
     /// Gets the name of the channel for sending a message to all collections associated with a user.
     /// </summary>
-    /// <param name="userId">The ID of the user to get the channel for.</param>
+    /// <param name="userId">The ID of the user to get the channel for.</param>32
     [MethodImpl(MethodImplOptions.AggressiveInlining)]
-    public string User(string userId) => Normalize($"{prefix}_user_{userId}");
+    public string User(string userId) => Normalize(prefix, $"user_{userId}");
 
     /// <summary>
     /// Gets the name of the acknowledgement channel for the specified server.
     /// </summary>
     /// <param name="serverName">The name of the server to get the acknowledgement channel for.</param>
     [MethodImpl(MethodImplOptions.AggressiveInlining)]
-    public string Ack(string serverName) => Normalize($"{prefix}_internal_ack_{serverName}");
+    public string Ack(string serverName) => Normalize(prefix, $"internal_ack_{serverName}");
 }
