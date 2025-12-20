4a1f7defdf2054deb65d62a264d1301ed513ecae
ianwold <ilwold@hotmail.com>
2025-12-20 11:37:40 -0600

    Add sha256 channel name normalization plus raw prefix

--- src/PostgresChannels.cs
+++ src/PostgresChannels.cs
@@ -11,26 +11,21 @@ internal sealed class PostgresChannels(string prefix, string returnServerName)
 
     public static string Normalize(string prefix, string name)
     {
-        var channelName = $"{prefix}{name}";
         Span<byte> utf8 = stackalloc byte[1024];
-        int utf8Length = Encoding.UTF8.GetBytes(channelName.AsSpan(), utf8);
+        var utf8Length = Encoding.UTF8.GetBytes(name.AsSpan(), utf8);
 
-        Span<byte> hash = stackalloc byte[48];
-        SHA384.HashData(utf8[..utf8Length], hash);
+        Span<byte> hash = stackalloc byte[32];
+        SHA256.HashData(utf8[..utf8Length], hash);
 
-        Span<byte> base64 = stackalloc byte[64];
-        Base64.EncodeToUtf8(hash[..47], base64, out _, out int written);
+        Span<byte> base64 = stackalloc byte[44];
+        Base64.EncodeToUtf8(hash, base64, out _, out var written);
 
-        return string.Create(written - 1, base64, static (destination, source) =>
+        return prefix + string.Create(written-1, base64.ToArray(), static (destination, source) =>
         {
             for (int i = 0; i < destination.Length; i++)
             {
-                destination[i] = source[i] switch
-                {
-                    (byte)'+' => '_',
-                    (byte)'/' => '_',
-                    var c => (char)c
-                };
+                var c = source[i];
+                destination[i] = c == (byte)'+' || c == (byte)'/' ? '_' : (char)c;
             }
         });
     }
