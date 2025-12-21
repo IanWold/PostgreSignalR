787ed0525456be6e748f0e5fc8c1c8c61babb09a
ianwold <ilwold@hotmail.com>
2025-12-21 15:34:24 -0600

    Add comments for custom table example

--- examples/PostgreSignalR.Examples.CustomTable/Program.cs
+++ examples/PostgreSignalR.Examples.CustomTable/Program.cs
@@ -14,8 +14,25 @@ builder.Services
     .AddPostgresBackplane(dataSource)
     .AddBackplaneTablePayloadStrategy(options =>
     {
+        // Storage mode determines when paylaods are written to the payload table.
+        // Always will write all payloads to the table,
+        //     Auto will only write large payloads to the table.
         options.StorageMode = PostgresBackplanePayloadTableStorage.Always;
+        // options.StorageMode = PostgresBackplanePayloadTableStorage.Auto;
+
+
+
+        // SchemaName and Table name should be configured to identify where
+        //     your table is:
+        options.SchemaName = null;
         options.TableName = "custom_payloads";
+
+
+
+        // This example shows how to set up a custom table with a pg_cron job
+        //     to provide automatic cleanup.
+        // In this case, AutomaticCleanup should be configured to false
+        //     so that the built-in automatic cleanup does not run.
         options.AutomaticCleanup = false;
     });
 
