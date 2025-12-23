e05b28672b5aa5ebca65e5b7e3e1a9993dc61c76
Ian Wold <ilwold@hotmail.com>
2025-12-23 18:36:32 +0000

    Enable comlex object tests

--- tests/PostgreSignalR.IntegrationTests.Abstractions/ComplexObject.cs
+++ tests/PostgreSignalR.IntegrationTests.Abstractions/ComplexObject.cs
@@ -1,7 +1,19 @@
+using System.Linq;
+
 namespace PostgreSignalR.IntegrationTests.Abstractions;
 
 public record ComplexObject(
     SimpleObject SimpleObjectProperty,
     List<SimpleObject> SimpleObjectsProperty,
     Dictionary<string, SimpleObject> SimpleObjectsDictionaryProperty
-);
+)
+{
+    public override bool Equals(object o) =>
+        o is ComplexObject c
+        && c.SimpleObjectProperty == SimpleObjectProperty
+        && c.SimpleObjectsProperty.SequenceEqual(SimpleObjectsProperty)
+        && c.SimpleObjectsDictionaryProperty.OrderBy(d => d).SequenceEqual(SimpleObjectsDictionaryProperty.OrderBy(d => d));
+
+    public override int GetHashCode() =>
+        HashCode.Combine(SimpleObjectProperty, SimpleObjectsProperty, SimpleObjectsDictionaryProperty);
+}
