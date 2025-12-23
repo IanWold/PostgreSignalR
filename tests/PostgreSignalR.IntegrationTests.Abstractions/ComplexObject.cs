07b0d3cef22a7f0034ed2621c61100beb530567f
Ian Wold <ilwold@hotmail.com>
2025-12-23 19:13:26 +0000

    Update complex object ordering

--- tests/PostgreSignalR.IntegrationTests.Abstractions/ComplexObject.cs
+++ tests/PostgreSignalR.IntegrationTests.Abstractions/ComplexObject.cs
@@ -8,8 +8,12 @@ public class ComplexObject
     public required List<SimpleObject> SimpleObjectsProperty { get; set; }
     public required Dictionary<string, SimpleObject> SimpleObjectsDictionaryProperty { get; set; }
 
-    public bool IsEqualTo(ComplexObject c) =>
-        c.SimpleObjectProperty == SimpleObjectProperty
+    public override bool Equals(object o) =>
+        o is ComplexObject c
+        && c.SimpleObjectProperty == SimpleObjectProperty
         && c.SimpleObjectsProperty.SequenceEqual(SimpleObjectsProperty)
-        && c.SimpleObjectsDictionaryProperty.OrderBy(d => d).SequenceEqual(SimpleObjectsDictionaryProperty.OrderBy(d => d));
+        && c.SimpleObjectsDictionaryProperty.OrderBy(d => d.Key).SequenceEqual(SimpleObjectsDictionaryProperty.OrderBy(d => d.Key));
+
+    public override int GetHashCode() =>
+        HashCode.Combine(SimpleObjectProperty, SimpleObjectsProperty, SimpleObjectsDictionaryProperty);
 }
