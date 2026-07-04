9bc7aa91c32f1271032795f277db2e104def7564
ianwold <ilwold@hotmail.com>
2026-07-04 09:58:23 -0500

    Try implementing timeout and trace upload to debug test hanging

--- tests/PostgreSignalR.IntegrationTests/Utilities/TestTimeouts.cs
+++ tests/PostgreSignalR.IntegrationTests/Utilities/TestTimeouts.cs
@@ -2,6 +2,7 @@ namespace PostgreSignalR.IntegrationTests;
 
 internal static class TestTimeouts
 {
+    public static readonly TimeSpan ConnectionStartTimeout = TimeSpan.FromSeconds(30);
     public static readonly TimeSpan DefaultMessageTimeout = TimeSpan.FromSeconds(1);
     public static readonly TimeSpan DisconnectSettleDelay = TimeSpan.FromMilliseconds(150);
     public const int HealthCheckMaxAttempts = 120;
