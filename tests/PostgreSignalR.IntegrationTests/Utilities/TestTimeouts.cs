4a0f06db4fdadf96a16e7731cef9b2431027588b
ianwold <ilwold@hotmail.com>
2026-07-04 10:54:58 -0500

    Double some timeouts

--- tests/PostgreSignalR.IntegrationTests/Utilities/TestTimeouts.cs
+++ tests/PostgreSignalR.IntegrationTests/Utilities/TestTimeouts.cs
@@ -3,10 +3,10 @@ namespace PostgreSignalR.IntegrationTests;
 internal static class TestTimeouts
 {
     public static readonly TimeSpan ConnectionStartTimeout = TimeSpan.FromSeconds(30);
-    public static readonly TimeSpan DefaultMessageTimeout = TimeSpan.FromSeconds(1);
-    public static readonly TimeSpan DisconnectSettleDelay = TimeSpan.FromMilliseconds(150);
+    public static readonly TimeSpan DefaultMessageTimeout = TimeSpan.FromSeconds(2);
+    public static readonly TimeSpan DisconnectSettleDelay = TimeSpan.FromMilliseconds(300);
     public const int HealthCheckMaxAttempts = 120;
     public static readonly TimeSpan HealthCheckPollInterval = TimeSpan.FromMilliseconds(50);
-    public static readonly TimeSpan NegativeAssertionTimeout = TimeSpan.FromMilliseconds(250);
+    public static readonly TimeSpan NegativeAssertionTimeout = TimeSpan.FromMilliseconds(500);
     public static readonly TimeSpan RetryAttemptTimeout = TimeSpan.FromSeconds(5);
 }
